48475b50cce266982bd81f7fd7e227ef4d497a36
Pham Minh Son <sonpm4@ghtk.co>
2024-04-09 15:46:11 +0700

    commit

--- src/test/java/com/example/test_java_spring/demo/HomeControllerTest.java
+++ src/test/java/com/example/test_java_spring/demo/HomeControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
